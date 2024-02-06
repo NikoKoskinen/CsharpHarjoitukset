413a8c89b25b25a4b9bcf59baa763923c581d699
NikoKoskinen <47375@edu.raseko.fi>
2024-02-06 14:29:38 +0200

    Added constructors with 3 overloads (zero, one, two, tree)

--- CSluokkaharjoitus/Program.cs
+++ CSluokkaharjoitus/Program.cs
@@ -8,10 +8,38 @@ namespace CSluokkaharjoitus
 {
     class Hooman
     {
-        // Define properties of Hooman
-        string name;
-        int age;
-        string gender;
+        // Define properties of Hooman ie. fields
+        string name ="essi esimerkki";
+        int age = 30;
+        string gender = "Emäntä";
+
+        // Constructor with zero arguments
+        Hooman()
+        {
+
+        }
+
+        // Constructor with one argument
+        Hooman(string name)
+        {
+            this.name = name;
+        }
+
+        // Constructor with two arguments
+        Hooman(string name, int age)
+        {
+            this.name=name;
+            this.age = age;
+        }
+
+        // Constructor with three arguments
+        Hooman(string name, int age, string gender)
+        {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+        }
+
     }
 
     internal class Program

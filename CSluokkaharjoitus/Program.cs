5a40bdab8df88da0f51c2a39a70d7654a368803b
NikoKoskinen <47375@edu.raseko.fi>
2024-02-06 15:18:27 +0200

    Created an object from Hooman class in the Main of Program class and tested it.

--- CSluokkaharjoitus/Program.cs
+++ CSluokkaharjoitus/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,43 +10,58 @@ namespace CSluokkaharjoitus
     class Hooman
     {
         // Define properties of Hooman ie. fields
-        string name ="essi esimerkki";
+        string name ="Essi Esimerkki";
         int age = 30;
         string gender = "Emäntä";
 
-        // Constructor with zero arguments
-        Hooman()
+        // Constructor with zero arguments (default constructor w/o args)
+        // No needed to define, will be created automatically
+        public Hooman()
         {
 
         }
 
         // Constructor with one argument
-        Hooman(string name)
+        public Hooman(string name)
         {
             this.name = name;
         }
 
         // Constructor with two arguments
-        Hooman(string name, int age)
+         public Hooman(string name, int age)
         {
             this.name=name;
             this.age = age;
         }
 
         // Constructor with three arguments
-        Hooman(string name, int age, string gender)
+        public Hooman(string name, int age, string gender)
         {
             this.name = name;
             this.age = age;
             this.gender = gender;
         }
 
+        // A method to say something
+        public void SayOpinion()
+        {
+            Console.WriteLine("Perkeleen humppi");
+
+        }
     }
 
     internal class Program
     {
         static void Main(string[] args)
         {
+            // Create (instantiate) a hooman object from Hooman class 
+            Hooman owner = new Hooman("Ossi Omistaja", 35, "isäntä");
+            
+            // call the SayOpinion method
+            owner.SayOpinion();
+
+            // keep the window open until Enter pressed
+            Console.ReadLine();
         }
     }
 }
